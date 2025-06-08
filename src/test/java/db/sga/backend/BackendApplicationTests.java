e5c51d086781d53416aae60938414ac3bc6b987f
JsLealM <johanleal2002@gmail.com>
2025-06-08 13:39:16 -0500

    First commit

---
+++ src/test/java/db/sga/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package db.sga.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
